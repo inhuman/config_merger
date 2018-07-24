79c4b1ffa96328d694943291e21c5d654dbdf2f5
id <msgexec@gmail.com>
2018-07-24 13:17:45 +0300

    added exit log

--- merger.go
+++ merger.go
@@ -6,6 +6,7 @@ import (
 	"reflect"
 	"fmt"
 	"sync"
+	"log"
 )
 
 type Merger struct {
@@ -66,14 +67,19 @@ func (m *Merger) RunWatch() error {
 		}
 	}
 
+	log.Println("Waiting stop watch call")
 	<- m.done
+	log.Println("Stop watch received, sending stop to watchers")
 
 	for d := range m.Sources {
+		log.Println("Sent stop to watcher", d)
 		doneMap[d] <- true
 	}
+
+	log.Println("Waiting wait group done")
 	wg.Wait()
 
-	fmt.Println("Exiting run watch")
+	log.Println("Exiting run watch")
 
 	return nil
 }
